310806d63ad569d08f31eb38da9975a9f52cd504
Ivan Mizernyi <mizernijivan39@gmail.com>
2026-08-25 21:12:35 +0300

    Initial commit: initial keyboard layout switcher logic

---
+++ Switcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using Switcher;
+using System;
+using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "TextFile1.txt";
+        var dictionaty = InitDictionary(filePath);
+
+        //Entering text
+        Console.Write("Enter text to convert: ");
+        string? unformattedText = Console.ReadLine();
+        string? formattedText = Switch(dictionaty, unformattedText) ?? "Can't recognize";
+
+        Console.Write("Result: ");
+        Console.WriteLine(formattedText);
+    }
+    public static Dictionary<char, char> InitDictionary(string filePath)
+    {
+        string json = File.ReadAllText(filePath);
+        var dict = JsonSerializer.Deserialize<Dictionary<char, char>>(json) ?? new();
+        return dict;
+    }
+    static public string? Switch(Dictionary<char, char> dict, string? unformattedText)
+    {
+        string formattedText;
+        StringBuilder stringBuilder = new StringBuilder();
+        SpecialChars specialChars = new SpecialChars();
+
+        if (dict != null)
+        {
+            bool? isLatin = specialChars.CountLatinKiril(unformattedText);
+            if (isLatin == null)
+                return null;
+
+            if (!isLatin.Value)
+            {
+                dict = dict.ToDictionary(pair => pair.Value, pair => pair.Key);
+            }
+
+            foreach (char ch in unformattedText)
+            {
+                if (dict.TryGetValue(ch, out char newChar))
+                {
+                    stringBuilder.Append(newChar);
+                }
+                else
+                {
+                    stringBuilder.Append(ch);
+                }
+            }
+            return formattedText = stringBuilder.ToString();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Switcher/SpecialChars.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switcher
+{
+    internal class SpecialChars
+    {
+        List<char> specialChars = "{}:\",[];'`./~<>?@#№$^&".ToList();
+        List<char> latinChars = "qwertyuiopasdfghjklzxcvbnm".ToList();
+        List<char> kirilChars = "йцукенгшщзхъфывапролджэячсмитьбюё".ToList();
+        private bool ExceptionChar(char excChar)//for special chars like dots, parenthess or math signs
+        {
+            if (specialChars.Contains(excChar)) 
+                return true;
+            else
+                return false;
+        }
+        public bool? CountLatinKiril(string inputText)//for counting the amount of l/k chars to choose dictionary
+        {
+            int Lcount = 0, Kcount = 0;
+
+            foreach (char c in inputText)
+            {
+                if (!ExceptionChar(c))
+                {
+                    Lcount += Convert.ToInt32(latinChars.Contains(c));
+                    Kcount += Convert.ToInt32(kirilChars.Contains(c));
+                }
+                else continue;
+            }
+
+            if (Lcount == Kcount && Lcount == 0)
+                return null;
+
+            return Lcount > Kcount ? true : false;
+        }
+    }
+}
